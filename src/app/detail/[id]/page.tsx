10bf9cb96debbbc91d2aae5d844f31f3c7e284a7
ws <853800186@qq.com>
2024-04-14 19:42:19 +0800

    制作detail静态页面ing

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,4 +1,32 @@
 'use client'
+import './index.css'
 export default function Detail() {
-  return <div>Detail</div>
+  return (
+    <>
+      <div className="detail-container">
+        <div className="detail-head">
+          <div className="word">chairman</div>
+        </div>
+        <div className="detail-body">
+          <div className="translate">
+            n. 主席，会长；董事长
+            <div className="soundmark">
+              <span>英:[ˈeniweər]</span>
+              <span>美:[ˈeniwer]</span>
+            </div>
+          </div>
+
+          <div className="distortion">
+            [ 复数 chairmen 过去式 chairmaned或-manned 过去分词
+            chairmaned或-manned 现在分词 chairmaning或-manning 第三人称单数
+            chairmans ]
+          </div>
+          <div className="phrase"></div>
+          <div className="samples">
+            <p>He was an admirable chairman.他是位值得尊敬的主席。</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
