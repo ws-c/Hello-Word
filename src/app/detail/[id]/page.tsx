ed3b9d44050baf931c20337c07d70f4088d359a6
ws <853800186@qq.com>
2024-04-15 21:22:38 +0800

    完善detail页面布局

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,5 +1,8 @@
 'use client'
+import { RightOutlined } from '@ant-design/icons'
 import './index.css'
+import Image from 'next/image'
+import icon from '@/assets/trumpet.png'
 export default function Detail() {
   return (
     <>
@@ -12,20 +15,25 @@ export default function Detail() {
             n. 主席，会长；董事长
             <div className="soundmark">
               <span>英:[ˈeniweər]</span>
+              <Image src={icon} alt="trumpet"></Image>
               <span>美:[ˈeniwer]</span>
+              <Image src={icon} alt="trumpet"></Image>
             </div>
           </div>
-
+          <div className="samples">
+            <p>例句：He was an admirable chairman.他是位值得尊敬的主席。</p>
+          </div>
           <div className="distortion">
-            [ 复数 chairmen 过去式 chairmaned或-manned 过去分词
+            <p>派生词：</p>[ 复数 chairmen 过去式 chairmaned或-manned 过去分词
             chairmaned或-manned 现在分词 chairmaning或-manning 第三人称单数
             chairmans ]
           </div>
-          <div className="phrase"></div>
-          <div className="samples">
-            <p>He was an admirable chairman.他是位值得尊敬的主席。</p>
+          <div className="phrase">
+            <p>词组短语：</p>
+            board chairmann. 董事长
           </div>
         </div>
+        <RightOutlined className="RightOutlined" />
       </div>
     </>
   )
