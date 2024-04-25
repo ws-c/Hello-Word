0733ec4bbb4838ca1e7abb218498d8d96440835a
ws <853800186@qq.com>
2024-04-25 16:01:27 +0800

    更换数据库

--- src/db/mysql.tsx
+++ src/db/mysql.tsx
@@ -1,21 +1,21 @@
 const mysql = require('mysql2/promise')
 
 // 创建全局的 MySQL 连接池
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'cet_4',
-  port: 3306,
-})
-
 // const pool = mysql.createPool({
-//   host: 'sql.wsfdb.cn',
-//   user: '853800186word',
-//   password: '118816742',
-//   database: '853800186word',
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root',
+//   database: 'cet_4',
 //   port: 3306,
 // })
+
+const pool = mysql.createPool({
+  host: 'sql.wsfdb.cn',
+  user: '853800186word',
+  password: '118816742',
+  database: '853800186word',
+  port: 3306,
+})
 
 // const pool = mysql.createPool({
 //   host: 'docker.maxd.gq',
